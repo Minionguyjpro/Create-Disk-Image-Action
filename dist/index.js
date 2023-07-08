429da3a2d54430066935865d39a21e77ec4fe384
Minionguyjpro <minionguyjpro@gmail.com>
2023-07-08 09:43:01 +0000

    And again...

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('child_process').exec;
+const artifactClient = require('artifact-client');
 
 async function run() {
   try {
