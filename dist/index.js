694b2d812e27b1ca2ee14e9c385d3d158a01b75e
Minionguyjpro <minionguyjpro@gmail.com>
2023-07-09 12:53:06 +0000

    TEST

--- dist/index.js
+++ dist/index.js
@@ -35,12 +35,13 @@ function run() {
     const isoFilePath = path.join(sourceFolderPath, filename);
 
     execSync(`choco install imgburn -y`);
-    const imgBurnPath = "'C:\\Program Files (x86)\\ImgBurn\\ImgBurn.exe'";
+    const imgBurnPath = `"C:\\Program Files (x86)\\ImgBurn\\ImgBurn.exe"`;
     const imgBurnArgs = `/MODE BUILD /BUILDINPUTMODE FOLDER /BUILDOUTPUTMODE IMAGEFILE /SRC "${sourceFolderPath}" /DEST "${outputDir}\\${filename}" /FILESYSTEM "ISO9660 + Joliet" /VOLUMELABEL "${label}" /OVERWRITE YES /START /CLOSE /NOIMAGEDETAILS`;
-
+    
     console.log(`Running ImgBurn with command: ${imgBurnPath} ${imgBurnArgs}`);
-
+    
     execSync(`"${imgBurnPath}" ${imgBurnArgs}`, { stdio: 'inherit' });
+    
 
     console.log('Disk image creation completed.');
 
