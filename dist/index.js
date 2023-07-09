4489222ce22e8fa7fed54fd2353a5be5fdbd82d2
Minionguyjpro <minionguyjpro@gmail.com>
2023-07-09 11:47:16 +0000

    Another test

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,12 @@ const path = require('path');
 
 async function run() {
   try {
-    const sourcePath = core.getInput('path');
-    const outputDir = core.getInput('output-dir');
+    const sourceFolder = core.getInput('path');
     const filename = core.getInput('filename');
+    const label = core.getInput('label');
+    const shouldPush = core.getInput('push').toLowerCase() === 'true';
+    const outputDir = core.getInput('output-dir');
+    const pushMsg = core.getInput('push-msg');
 
     console.log('Doing prerequisites...');
 
@@ -20,34 +23,31 @@ async function run() {
     const cloneDir = '/github/workspace';
     await exec(`choco install imgburn -y`);
     await exec(`git clone https://github.com/${github.context.repo.owner}/${github.context.repo.repo}.git ${cloneDir}`);
-    process.chdir(cloneDir);
 
-    console.log('Cloned repository contents:');
-    const { stdout: lsOutput } = await exec('ls -la');
-    console.log(lsOutput);
+    console.log('Setting up ImgBurn...');
 
-    const examplePath = path.join(cloneDir, 'example');
-    console.log(`Contents of the 'example' folder:`);
-    const { stdout: exampleLsOutput } = await exec(`ls -la ${examplePath}`);
-    console.log(exampleLsOutput);
-
-    console.log('Copying test.iso file...');
-    const imgBurnTestIsoPath = path.join(examplePath, 'test.iso');
-    const destinationPath = path.join(cloneDir, filename);
-    fs.copyFileSync(imgBurnTestIsoPath, destinationPath);
-    console.log('test.iso file copied successfully.');
-
-    console.log('Creating disk image...');
     const imgBurnPath = `"${process.env['ProgramFiles(x86)']}\\ImgBurn\\ImgBurn.exe"`;
-    const imgBurnArgs = `/MODE BUILD /BUILDINPUTMODE STANDARD /BUILDOUTPUTMODE IMAGEFILE /SRC "${sourcePath}" /DEST "${outputDir}/${filename}" /FILESYSTEM "ISO9660 + Joliet" /OVERWRITE YES /ROOTFOLDER YES /START /CLOSE /NOIMAGEDETAILS`;
+    const imgBurnArgs = `/MODE BUILD /BUILDINPUTMODE FOLDER /BUILDOUTPUTMODE IMAGEFILE /SRC "${sourceFolder}" /DEST "${outputDir}\\${filename}" /FILESYSTEM "ISO9660 + Joliet" /VOLUMELABEL "${label}" /OVERWRITE YES /START /CLOSE /NOIMAGEDETAILS`;
+
     console.log(`Running ImgBurn with command: ${imgBurnPath} ${imgBurnArgs}`);
 
     const { stdout, stderr } = await exec(`PowerShell -Command "& {${imgBurnPath} ${imgBurnArgs}}"`);
 
+    console.log('Disk image creation completed.');
+
+    if (shouldPush) {
+      console.log('Pushing changes to Git...');
+      process.chdir(cloneDir);
+      await exec('git add .');
+      await exec(`git commit -m "${pushMsg}"`);
+      await exec('git push');
+      console.log('Changes pushed to Git.');
+    }
+
     console.log('Uploading disk image as artifact...');
     const artifactClient = artifact.create();
     const artifactName = 'disk-image';
-    const files = [`${outputDir}/${filename}`];
+    const files = [`${outputDir}\\${filename}`];
     if (!fs.existsSync(outputDir)) {
       fs.mkdirSync(outputDir);
     }
@@ -56,6 +56,9 @@ async function run() {
       continueOnError: false
     };
     const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
+
+    console.log('Disk image artifact uploaded.');
+
   } catch (error) {
     core.setFailed(error.message);
   }
