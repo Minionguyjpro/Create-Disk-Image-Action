ec0328693b8addcc7a3261edfae8ade5a6e0f968
Minionguyjpro <minionguyjpro@gmail.com>
2023-07-09 12:16:14 +0000

    TEST

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
 const artifact = require('@actions/artifact');
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
-async function run() {
+function run() {
   try {
     const sourceFolder = core.getInput('path');
     const filename = core.getInput('filename');
@@ -17,37 +16,43 @@ async function run() {
 
     console.log('Doing prerequisites...');
 
-    await exec(`git config --global user.email "${github.context.payload.pusher.email}"`);
-    await exec(`git config --global user.name "${github.context.payload.pusher.name}"`);
+    execSync('git config --global user.email "<your-email>"');
+    execSync('git config --global user.name "<your-name>"');
 
-    const cloneDir = '/github/workspace';
-    await exec(`choco install imgburn -y`);
-    await exec(`git clone https://github.com/${github.context.repo.owner}/${github.context.repo.repo}.git ${cloneDir}`);
+    const repoName = github.context.repo.repo;
+    const cloneDir = path.join('D:\\REPO', repoName);
+    if (!fs.existsSync(cloneDir)) {
+      fs.mkdirSync(cloneDir, { recursive: true });
+    }
+    const cloneCmd = `git clone https://github.com/${github.context.repo.owner}/${repoName}.git ${cloneDir}`;
+    execSync(cloneCmd, { stdio: 'inherit' });
 
     console.log('Setting up ImgBurn...');
 
+    process.chdir(cloneDir); // Change to the cloned repository directory
+
     const sourceFolderPath = path.join(cloneDir, sourceFolder);
-    const isoFilePath = path.join(sourceFolderPath, 'test.iso');
+    const isoFilePath = path.join(sourceFolderPath, filename);
 
     if (!fs.existsSync(isoFilePath)) {
       throw new Error(`File ${isoFilePath} does not exist.`);
     }
 
-    const imgBurnPath = `"${process.env['ProgramFiles(x86)']}\\ImgBurn\\ImgBurn.exe"`;
+    execSync(`choco install imgburn -y`);
+    const imgBurnPath = `"C:\\Program Files (x86)\\ImgBurn\\ImgBurn.exe"`;
     const imgBurnArgs = `/MODE BUILD /BUILDINPUTMODE FOLDER /BUILDOUTPUTMODE IMAGEFILE /SRC "${sourceFolderPath}" /DEST "${outputDir}\\${filename}" /FILESYSTEM "ISO9660 + Joliet" /VOLUMELABEL "${label}" /OVERWRITE YES /START /CLOSE /NOIMAGEDETAILS`;
 
     console.log(`Running ImgBurn with command: ${imgBurnPath} ${imgBurnArgs}`);
 
-    const { stdout, stderr } = await exec(`PowerShell -Command "& {${imgBurnPath} ${imgBurnArgs}}"`);
+    execSync(`"${imgBurnPath}" ${imgBurnArgs}`, { stdio: 'inherit' });
 
     console.log('Disk image creation completed.');
 
     if (shouldPush) {
       console.log('Pushing changes to Git...');
-      process.chdir(cloneDir);
-      await exec('git add .');
-      await exec(`git commit -m "${pushMsg}"`);
-      await exec('git push');
+      execSync('git add .');
+      execSync(`git commit -m "${pushMsg}"`);
+      execSync('git push');
       console.log('Changes pushed to Git.');
     }
 
@@ -56,13 +61,13 @@ async function run() {
     const artifactName = 'disk-image';
     const files = [`${outputDir}\\${filename}`];
     if (!fs.existsSync(outputDir)) {
-      fs.mkdirSync(outputDir);
+      fs.mkdirSync(outputDir, { recursive: true });
     }
     const rootDirectory = outputDir;
     const options = {
       continueOnError: false
     };
-    const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
+    const uploadResult = artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
 
     console.log('Disk image artifact uploaded.');
 
